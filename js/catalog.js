621f88838aabaf423c26534c628d093bed2f3fe4
Hunter Fehr <hunter.fehr3935@yahoo.com>
2022-09-29 16:51:23 -0700

    local storage attempt

--- js/catalog.js
+++ js/catalog.js
@@ -12,6 +12,10 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in Product.allProducts) {
+    let option = document.createElement('option');
+    option.value = Product.allProducts[i].name;
+    option.textContent = Product.allProducts[i].name;
+    selectElement.appendChild(option);
 
   }
 
@@ -23,7 +27,7 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
-
+event.preventDefault();
   // Do all the things ...
   addSelectedItemToCart();
   cart.saveToLocalStorage();
@@ -35,8 +39,13 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  let item = document.getElementById('items').value;
+  console.log('item', item);
   // TODO: get the quantity
+  let quantity = document.getElementById('quantity').value;
+  console.log('quantity', item);
   // TODO: using those, add one item to the Cart
+  cart.addItem(item, quantity);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
